45dd833b0633b23e27920e10bc30b07b3a408195
nrohrbach <52918447+nrohrbach@users.noreply.github.com>
2022-12-23 15:35:53 +0100

    Version 23.12.2022

--- Sharedmobility_Dashboard.py
+++ Sharedmobility_Dashboard.py
@@ -1,9 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import requests
 import datetime as dt
 import numpy as np
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+
+
+# In[2]:
+
 
 # Providers.json abfragen
 url = 'https://www.sharedmobility.ch/providers.json'
@@ -16,6 +26,11 @@ name = [s['name'] for s in data['data']['providers']]
 # Create Dataframe providers
 header = ['provider_id', 'vehicle_type','name','last_updated']
 providers = pd.DataFrame(list(zip(provider_id, vehicle_type, name, last_updated)),columns =['provider_id', 'Type','Name','last_updated'])
+#providers
+
+
+# In[33]:
+
 
 # Dataframe mit Aktualisierungsstand vorbereiten
 # LastUpdated als Zeitobjekt
@@ -29,6 +44,10 @@ providers['DelayMinutes'] = round(providers['delay']/np.timedelta64(1,'m'),2)
 #Sortiere nach DelayMinutes
 providers = providers.sort_values('DelayMinutes', ascending=False)
 providers_Cleaned = providers[['provider_id', 'Type', 'DelayMinutes']]
+
+
+# In[34]:
+
 
 # Funktion welche Zellen farblich hervorhebt.
 def colorcells(val):
@@ -44,7 +63,23 @@ def colorcells(val):
     return 'background-color: %s' % color
 
 providers_Cleaned = providers_Cleaned.style.applymap(colorcells, subset = ['DelayMinutes'])
+#providers_Cleaned
 
+
+# In[3]:
+
+
+# LastUpdated als Zeitobjekt
+providers['last_updated'] = pd.to_datetime(providers['last_updated'],unit='s')
+
+# Der jetztige Zeitstand in UTC
+now = dt.datetime.utcnow()
+
+# Berechnen wie lange seit letztem Update
+providers['delay'] = str(now - providers['last_updated'])
+
+
+# In[21]:
 
 
 # Free_bikes_status.json abfragen
@@ -63,19 +98,7 @@ header = ['location_id', 'provider_id','lat', 'lon','num_bikes_available']
 free_bikes = pd.DataFrame(list(zip(location_id, provider_id, lat, lon, num_bikes_available,)),columns =['location_id', 'provider_id', 'lat', 'lon','num_bikes_available'])
 
 
-# In[34]:
-
-
-#free_bikes
-
-
-# In[36]:
-
-
-#free_bikes['provider_id'].value_counts().reset_index()
-
-
-# In[37]:
+# In[24]:
 
 
 # Station_information.json abfragen
@@ -108,51 +131,71 @@ location = pd.concat([stations, free_bikes])
 locations = pd.merge(location, providers, on="provider_id")
 
 
-# In[38]:
+# In[69]:
 
 
 locations['num_bikes_available'] = locations['num_bikes_available'].astype(int)
 #locations
 
 
-# In[84]:
-
-
-ProviderAssets = locations.groupby(['provider_id'])['num_bikes_available'].sum().reset_index()
+ProviderAssets = locations.groupby(['provider_id','Type'])['num_bikes_available'].sum().reset_index()
 ProviderAssets = ProviderAssets.sort_values('num_bikes_available', ascending=False)
-#ProviderAssets
+ProviderAssets.columns = ['Anbieter','Typ','Anzahl']
+
+
+# In[70]:
+
+
+CarAssets = ProviderAssets.query("Typ in['Car','E-Car']")
+BikeAssets = ProviderAssets.query("Typ in['Bike','E-Bike']")
+ScooterAssets = ProviderAssets.query("Typ in['Scooter','E-Scooter']")
+
+
+# In[91]:
+
+
+# Barcharts definieren
+barBikeAssets = px.bar(BikeAssets,x='Anbieter', y='Anzahl')
+barCarAssets = px.bar(CarAssets,x='Anbieter', y='Anzahl')
+barScooterAssets = px.bar(ScooterAssets,x='Anbieter', y='Anzahl')
 
 
 # In[85]:
 
 
 st.title('Shared Mobility Dashboard')
-st.markdown('''Dieses Dashboard zeigt die Verfügbarkeit und Aktualität der Daten der Shared Mobility Provider auf sharedmobility.ch''')
+st.markdown('''Dieses Dashboard gibt eine Übersicht der Inhalte und der Aktualität der Daten Shared Mobility Provider welche auf sharedmobility.ch gezeigt werden.''')
 
 
 # In[86]:
 
 
 st.header('Übersicht der Datenaktualität')
-st.dataframe(providers_Cleaned)
+st.dataframe(providers_sort)
 
 
-# In[87]:
+# In[92]:
 
 
-st.header('Naechstes Kapitel')
+#Barchart Bikes
+st.header('Anzahl Bikes')
+st.plotly_chart(barBikeAssets)
 
 
-# In[88]:
+# In[93]:
 
 
-barProviderAssets = px.bar(ProviderAssets,x='provider_id', y='num_bikes_available')
+#Barchart Scooter
+st.header('Anzahl E-Scooter')
+st.plotly_chart(barScooterAssets)
 
 
-# In[89]:
+# In[94]:
 
 
-st.plotly_chart(barProviderAssets)
+#Barchart Car
+st.header('Anzahl Autos')
+st.plotly_chart(barCarAssets)
 
 
 # In[ ]:
